87cdaf7ce70ba30c822bfbf8c2d89fc2f9611789
unknown <jo.jcat@gmail.com>
2014-07-10 22:48:59 -0400

    Moved the initialization into the constructor from an external function.  Added a static offset for the teacher canvas and the 'helper preview' which is locked on by default.

--- core/src/com/josephcatrambone/rageofpainting/states/PlayState.java
+++ core/src/com/josephcatrambone/rageofpainting/states/PlayState.java
@@ -17,6 +17,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.josephcatrambone.rageofpainting.Game;
 import com.josephcatrambone.rageofpainting.entities.Button;
 import com.josephcatrambone.rageofpainting.entities.Canvas;
@@ -27,6 +28,7 @@ import com.josephcatrambone.rageofpainting.handlers.InputManager;
 import com.josephcatrambone.rageofpainting.handlers.TweenManager;
 
 public class PlayState extends GameState {
+	private static final Vector2 teacherCanvasLocation = new Vector2(40, 80);
 	
 	private BitmapFont font;
 	private SpriteBatch batch;
@@ -43,6 +45,7 @@ public class PlayState extends GameState {
 	private String[] hostComments; // What will our host say?
 	
 	// Demonstration pieces
+	private boolean showHint = true;
 	private Texture goalImage = null;
 	private Texture teacherImage = null;
 	private Pixmap teacherCanvas = null;
@@ -55,9 +58,11 @@ public class PlayState extends GameState {
 	// User pieces
 	private Canvas userCanvas;
 	private Button[] colorSelection;
+	private Button[] controls;
 	
 	public PlayState(String scriptFilename) {
 		super();
+		final int TOOLBAR_HEIGHT = 32;
 		camera = Game.mainCamera;
 		batch = Game.spriteBatch;
 		font = Game.font;
@@ -72,9 +77,80 @@ public class PlayState extends GameState {
 		lastScriptMarker = 0;
 		
 		// Convert the script filename into the actual script.
-		BufferedReader fin;
 		try {
-			parseScript(Gdx.files.internal(scriptFilename).read());
+			InputStream fstream = Gdx.files.internal(scriptFilename).read();
+			Scanner fin = new Scanner(fstream);
+			
+			// The file format:
+			//0 EpisodeTemplate // Level Name
+			//1 ImageFilename.png // Image to be loaded
+			//2 number_of_colors // Number of colors on the palette
+			//3 time_limit // How long the user has to complete it
+			//4 0.7 // This is the similarity that the images must have for this level to be passed
+			//5.0 0.0 // This is the percent of the way done we are
+			//5.1 thestate_for_the_painter_to_have.png Or Blank for no change.
+			//5.2 This is a line which the robot will say. 
+			//Repeat 5
+			
+			//JsonParser json = Json.createparser(new InputStream(new URL()));
+			
+			// Set level name
+			String levelName = fin.nextLine();
+			
+			// Set level image
+			String imageFilename = fin.nextLine();
+			System.out.println("Opening image.");
+			Pixmap img = new Pixmap(Gdx.files.internal(imageFilename));
+			teacherCanvas = new Pixmap(img.getWidth(), img.getHeight(), Format.RGBA8888);
+			teacherImage = new Texture(teacherCanvas);
+			userCanvas = new Canvas(Game.VIRTUAL_WIDTH/2, TOOLBAR_HEIGHT, img.getWidth(), img.getHeight()); // TODO: Move over the canvas to fit the aspect ratio.
+			userCanvas.brushSize = 3;
+			userCanvas.INTERPOLATION_LEVEL = 10;
+			
+			// Select the number of colors
+			int numColors = Integer.parseInt(fin.nextLine());
+			System.out.println("Selecting palette.");
+			pal = ImageToolkit.getPalette(img, numColors, 100);
+			
+			// Now that we have the level image and number of colors, process it.
+			System.out.println("Reducing colors.");
+			ImageToolkit.reduceColors(img, pal, img);
+			System.out.println("Finding steps.");
+			steps = ImageToolkit.approximateImage(img, pal);
+			System.out.println("Pushing texture from CPU mem to GPU mem.");
+			goalImage = new Texture(img);
+			System.out.println("Disposing of unused pixel map.");
+			img.dispose();
+			
+			// Set the time limit
+			episodeDuration = Float.parseFloat(fin.nextLine());
+			accumulatedTime = 0;
+			
+			// Set the passing threshold
+			passThreshold = Float.parseFloat(fin.nextLine());
+			
+			// Finish processing.
+			ArrayList <Float> triggers = new ArrayList<Float>();
+			ArrayList <String> emotions = new ArrayList<String>();
+			ArrayList <String> comments = new ArrayList<String>();
+			while(fin.hasNextLine()) {
+				triggers.add(Float.parseFloat(fin.nextLine()));
+				emotions.add(fin.nextLine());
+				comments.add(fin.nextLine());
+			}
+			timeMarkers = new Float[triggers.size()];
+			triggers.toArray(timeMarkers);
+			hostEmotions = new String[emotions.size()];
+			emotions.toArray(hostEmotions);
+			hostComments = new String[comments.size()];
+			comments.toArray(hostComments);
+
+			//playButton = new Button(goalImage, 0, 0, new Runnable() { public void run() {}; });
+			//TweenManager.addTween(playButton.position, "x", -1, 1, 10, TweenManager.makeTween(playButton.position, "x", 1, -1, 10, null));
+			
+			loaded = true;
+			fin.close();
+			fstream.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -85,83 +161,34 @@ public class PlayState extends GameState {
 			loaded = false;
 		}
 		
-		textOut.setText(hostComments[0]);
-	}
-
-	public void parseScript(InputStream fstream) throws IOException {
-		final int PALETTE_SELECTION_HEIGHT = 32;
-		Scanner fin = new Scanner(fstream);
-		
-		// The file format:
-		//0 EpisodeTemplate // Level Name
-		//1 ImageFilename.png // Image to be loaded
-		//2 number_of_colors // Number of colors on the palette
-		//3 time_limit // How long the user has to complete it
-		//4 0.7 // This is the similarity that the images must have for this level to be passed
-		//5.0 0.0 // This is the percent of the way done we are
-		//5.1 thestate_for_the_painter_to_have.png Or Blank for no change.
-		//5.2 This is a line which the robot will say. 
-		//Repeat 5
-		
-		//JsonParser json = Json.createparser(new InputStream(new URL()));
-		
-		// Set level name
-		String levelName = fin.nextLine();
-		
-		// Set level image
-		String imageFilename = fin.nextLine();
-		System.out.println("Opening image.");
-		Pixmap img = new Pixmap(Gdx.files.internal(imageFilename));
-		teacherCanvas = new Pixmap(img.getWidth(), img.getHeight(), Format.RGBA8888);
-		userCanvas = new Canvas(Game.VIRTUAL_WIDTH/2, PALETTE_SELECTION_HEIGHT, img.getWidth(), img.getHeight()); // TODO: Move over the canvas to fit the aspect ratio.
-		userCanvas.brushSize = 3;
-		userCanvas.INTERPOLATION_LEVEL = 10;
-		
-		// Select the number of colors
-		int numColors = Integer.parseInt(fin.nextLine());
-		System.out.println("Selecting palette.");
-		pal = ImageToolkit.getPalette(img, numColors, 100);
-		setupToolbox(Game.VIRTUAL_WIDTH/2, 0, Game.VIRTUAL_WIDTH/2, PALETTE_SELECTION_HEIGHT);
-		
-		// Now that we have the level image and number of colors, process it.
-		System.out.println("Reducing colors.");
-		ImageToolkit.reduceColors(img, pal, img);
-		System.out.println("Finding steps.");
-		steps = ImageToolkit.approximateImage(img, pal);
-		System.out.println("Pushing texture from CPU mem to GPU mem.");
-		goalImage = new Texture(img);
-		System.out.println("Disposing of unused pixel map.");
-		img.dispose();
-		
-		// Set the time limit
-		episodeDuration = Float.parseFloat(fin.nextLine());
-		accumulatedTime = 0;
-		
-		// Set the passing threshold
-		passThreshold = Float.parseFloat(fin.nextLine());
-		
-		// Finish processing.
-		ArrayList <Float> triggers = new ArrayList<Float>();
-		ArrayList <String> emotions = new ArrayList<String>();
-		ArrayList <String> comments = new ArrayList<String>();
-		while(fin.hasNextLine()) {
-			triggers.add(Float.parseFloat(fin.nextLine()));
-			emotions.add(fin.nextLine());
-			comments.add(fin.nextLine());
+		// Create our palette buttons
+		colorSelection = new Button[pal.length]; // All the colors plus brush enlarge and brush shrink.
+		int buttonWidth = (Game.VIRTUAL_WIDTH/2)/colorSelection.length;
+		for(int i=0; i < colorSelection.length; i++) {
+			Pixmap pm = new Pixmap(buttonWidth, TOOLBAR_HEIGHT, Format.RGBA8888);
+			pm.setColor(pal[i]);
+			pm.fillRectangle(0, 0, buttonWidth, TOOLBAR_HEIGHT);
+			Texture buttonTexture = new Texture(pm);
+			final Integer color = pal[i];
+			colorSelection[i] = new Button(buttonTexture, i*buttonWidth+(Game.VIRTUAL_WIDTH/2), 0, new Runnable() {
+				public void run() {
+					userCanvas.brushColor = color;
+				}
+			});
+			pm.dispose();
 		}
-		timeMarkers = new Float[triggers.size()];
-		triggers.toArray(timeMarkers);
-		hostEmotions = new String[emotions.size()];
-		emotions.toArray(hostEmotions);
-		hostComments = new String[comments.size()];
-		comments.toArray(hostComments);
-
-		//playButton = new Button(goalImage, 0, 0, new Runnable() { public void run() {}; });
-		//TweenManager.addTween(playButton.position, "x", -1, 1, 10, TweenManager.makeTween(playButton.position, "x", 1, -1, 10, null));
 		
-		loaded = true;
-		fin.close();
-		fstream.close();
+		// Create our brush and sequence control buttons
+		controls = new Button[0];
+		/*
+		controls[0] = new Button(null, 0, 0, new Runnable() {
+			public void run() {
+				
+			}
+		});
+		*/
+		
+		textOut.setText(hostComments[0]);
 	}
 
 	@Override
@@ -210,6 +237,11 @@ public class PlayState extends GameState {
 		for(Button b : colorSelection) {
 			b.update(dt);
 		}
+		
+		// Handle controls
+		for(Button b : controls) {
+			b.update(dt);
+		}
 	}
 
 	@Override
@@ -224,12 +256,17 @@ public class PlayState extends GameState {
 		batch.begin();
 		
 		// Render teacher images
-		//batch.draw(goalImage, 0f, 0f); // Draw goal image with mild transparency on teacher image.
-		if(teacherImage != null) { batch.draw(teacherImage, 0, 0); } //We only update the teacher image once in a while, so don't regernerate it every time.
+		batch.draw(teacherImage, teacherCanvasLocation.x-(teacherImage.getWidth()/2), teacherCanvasLocation.y-(teacherImage.getHeight()/2));
+		if(showHint) {
+			batch.draw(goalImage, teacherCanvasLocation.x-(teacherImage.getWidth()/2), teacherCanvasLocation.y-(teacherImage.getHeight()/2));
+		}
 		
 		// Render UI
 		userCanvas.render(batch);
 		for(Button b : colorSelection) {
+			b.render(batch);
+		}
+		for(Button b : controls) {
 			b.render(batch);
 		}
 		
