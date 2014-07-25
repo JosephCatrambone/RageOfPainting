f5c2f7d74a36a1d4ec2560a44dcff068f7a06564
unknown <jo.jcat@gmail.com>
2014-07-24 21:34:40 -0400

    Bring everything up to date.  Add missing level files and states.

--- desktop/src/com/josephcatrambone/rageofpainting/desktop/DesktopLauncher.java
+++ desktop/src/com/josephcatrambone/rageofpainting/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.josephcatrambone.rageofpainting.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Game.VIRTUAL_WIDTH*Game.SCREEN_SCALE;
+		config.height = Game.VIRTUAL_HEIGHT*Game.SCREEN_SCALE;
 		new LwjglApplication(new Game(), config);
 	}
 }
